196a8bc3397c2cc6671fc3de875ac18254e6c454
NegativeCoder01 <Negativestudiomain@gmail.com>
2025-05-12 18:56:21 -0600

    Added Number Combinations to repository

---
+++ NumbersAndMath/NumberCombinations/Program.cs
@@ -0,0 +1,406 @@
+﻿//Yes I know there are a bunch of problems
+//But if it works, dont fix it, the programmer way.
+
+using System;
+using System.Diagnostics.Tracing;
+using System.Linq.Expressions;
+using System.Net;
+
+namespace numbers
+{
+    public class Program
+    {
+        private const bool V = true;
+
+        public static void Main()
+        {
+            Console.Write("Enter the number of numbers that are in the comination (18 max): ");
+            int input = Convert.ToInt32(Console.ReadLine());
+            int num = 0;
+            int v = 0;
+            long numLong = v;
+
+            if (input==1)
+            {
+                num = 0;
+
+                bool n = false;
+
+                while (n=true)
+                {
+                    switch (num)
+                    {
+                        case < 11:
+                            n = true;
+                            Console.WriteLine(v);
+                            v = num++;
+                            break;
+                        case >= 10:
+                            n = false;
+                            break;
+                    }
+                }
+            }
+            else if (input==2)
+            {
+                num = 00;
+
+                bool n = false;
+
+                while (n=true)
+                {
+                    switch (num)
+                    {
+                        case < 101:
+                            n = true;
+                            Console.WriteLine(v);
+                            v = num++;
+                            break;
+                        case >= 100:
+                            n = false;
+                            break;
+                    }
+                }
+            }
+            else if (input==3)
+            {
+                num = 000;
+
+                bool n = false;
+
+                while (n=true)
+                {
+                    switch (num)
+                    {
+                        case < 1001:
+                            n = true;
+                            Console.WriteLine(v);
+                            v = num++;
+                            break;
+                        case >= 1000:
+                            n = false;
+                            break;
+                    }
+                }
+            }
+            else if (input==4)
+            {
+                num = 0000;
+
+                bool n = false;
+
+                while (n=true)
+                {
+                    switch (num)
+                    {
+                        case < 10001:
+                            n = true;
+                            Console.WriteLine(v);
+                            v = num++;
+                            break;
+                        case >= 10000:
+                            n = false;
+                            break;
+                    }
+                }
+            }
+            else if (input==5)
+            {
+                num = 00000;
+
+                bool n = false;
+
+                while (n=true)
+                {
+                    switch (num)
+                    {
+                        case < 100001:
+                            n = true;
+                            Console.WriteLine(v);
+                            v = num++;
+                            break;
+                        case >= 100000:
+                            n = false;
+                            break;
+                    }
+                }
+            }
+            else if (input==6)
+            {
+                num = 000000;
+
+                bool n = false;
+
+                while (n=true)
+                {
+                    switch (num)
+                    {
+                        case < 1000001:
+                            n = true;
+                            Console.WriteLine(v);
+                            v = num++;
+                            break;
+                        case >= 1000000:
+                            n = false;
+                            break;
+                    }
+                }
+            }else if (input==7)
+            {
+                num = 0000000;
+
+                bool n = false;
+
+                while (n=true)
+                {
+                    switch (num)
+                    {
+                        case < 10000001:
+                            n = true;
+                            Console.WriteLine(v);
+                            v = num++;
+                            break;
+                        case >= 10000000:
+                            n = false;
+                            break;
+                    }
+                }
+            }
+            else if (input==8)
+            {
+                num = 00000000;
+
+                bool n = false;
+
+                while (n=true)
+                {
+                    switch (num)
+                    {
+                        case < 100000001:
+                            n = true;
+                            Console.WriteLine(v);
+                            v = num++;
+                            break;
+                        case >= 100000000:
+                            n = false;
+                            break;
+                    }
+                }
+            }
+            else if (input==9)
+            {
+                num = 000000000;
+
+                bool n = false;
+
+                while (n=true)
+                {
+                    switch (num)
+                    {
+                        case < 1000000001:
+                            n = true;
+                            Console.WriteLine(v);
+                            v = num++;
+                            break;
+                        case >= 1000000000:
+                            n = false;
+                            break;
+                    }
+                }
+            }
+            else if (input==10)
+            {
+                num = 0000000000;
+
+                bool n = false;
+
+                while (n=true)
+                {
+                    switch (numLong)
+                    {
+                        case < 10000000001:
+                            n = true;
+                            Console.WriteLine(v);
+                            v = num++;
+                            break;
+                        case >= 100:
+                            n = false;
+                            break;
+                    }
+                }
+            }
+            else if (input==11)
+            {
+                num = 00000000000;
+
+                bool n = false;
+
+                while (n=true)
+                {
+                    switch (numLong)
+                    {
+                        case < 100000000001:
+                            n = true;
+                            Console.WriteLine(v);
+                            v = num++;
+                            break;
+                        case >= 100000000000:
+                            n = false;
+                            break;
+                    }
+                }
+            }
+            else if (input==12)
+            {
+                num = 000000000000;
+
+                bool n = false;
+
+                while (n=true)
+                {
+                    switch (numLong)
+                    {
+                        case < 1000000000001:
+                            n = true;
+                            Console.WriteLine(v);
+                            v = num++;
+                            break;
+                        case >= 1000000000000:
+                            n = false;
+                            break;
+                    }
+                }
+            }
+            else if (input==13)
+            {
+                num = 0000000000000;
+
+                bool n = false;
+
+                while (n=true)
+                {
+                    switch (numLong)
+                    {
+                        case < 10000000000001:
+                            n = true;
+                            Console.WriteLine(v);
+                            v = num++;
+                            break;
+                        case >= 10000000000000:
+                            n = false;
+                            break;
+                    }
+                }
+            }
+            else if (input==14)
+            {
+                num = 00000000000000;
+
+                bool n = false;
+
+                while (n=true)
+                {
+                    switch (numLong)
+                    {
+                        case < 100000000000001:
+                            n = true;
+                            Console.WriteLine(v);
+                            v = num++;
+                            break;
+                        case >= 100000000000000:
+                            n = false;
+                            break;
+                    }
+                }
+            }
+            else if (input==15)
+            {
+                num = 000000000000000;
+
+                bool n = false;
+
+                while (n=true)
+                {
+                    switch (numLong)
+                    {
+                        case < 1000000000000001:
+                            n = true;
+                            Console.WriteLine(v);
+                            v = num++;
+                            break;
+                        case >= 1000000000000000:
+                            n = false;
+                            break;
+                    }
+                }
+            }
+            else if (input==16)
+            {
+                num = 0000000000000000;
+
+                bool n = false;
+
+                while (n=true)
+                {
+                    switch (numLong)
+                    {
+                        case < 10000000000000001:
+                            n = true;
+                            Console.WriteLine(v);
+                            v = num++;
+                            break;
+                        case >= 10000000000000000:
+                            n = false;
+                            break;
+                    }
+                }
+            }
+            else if (input==17)
+            {
+                num = 00000000000000000;
+
+                bool n = false;
+
+                while (n=true)
+                {
+                    switch (numLong)
+                    {
+                        case < 100000000000000001:
+                            n = true;
+                            Console.WriteLine(v);
+                            v = num++;
+                            break;
+                        case >= 100000000000000000:
+                            n = false;
+                            break;
+                    }
+                }
+            }
+            else if (input==18)
+            {
+                num = 000000000000000000;
+
+                bool n = false;
+
+                while (n=true)
+                {
+                    switch (numLong)
+                    {
+                        case < 1000000000000000001:
+                            n = true;
+                            Console.WriteLine(v);
+                            v = num++;
+                            break;
+                        case >= 1000000000000000000:
+                            n = false;
+                            break;
+                    }
+                }
+            }
+            if (input>=19)
+            {
+                Console.WriteLine("Due to me being to lazy I am not finding a way to convert long or int variable into ulong right now, so the max limit is 18, sorry!");
+            }
+        }
+    }
+}
