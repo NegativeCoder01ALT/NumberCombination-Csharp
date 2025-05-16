ced4f08652647349d3f6a565a04394b3a6935cf1
NegativeCoder01ALT <negativestudiomain.alt@gmail.com>
2025-05-16 20:26:54 +0000

    done

--- Csharp/Blight/README.md
+++
@@ -1,1 +0,0 @@
-<img src="Logo/v1/v1b.png" alt="Blight Logo Full (v1b)" width="250" height="100">
---
+++ NumberCombinations/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace numbers
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            while (true)
+            {
+                Console.Write("Enter the number of digits in the combination (1 to 18): ");
+                if (!int.TryParse(Console.ReadLine(), out int digits) || digits < 1 || digits > 18)
+                {
+                    Console.WriteLine("Invalid input. Please enter a number between 1 and 18.");
+                    return;
+                }
+
+                long max = (long)Math.Pow(10, digits);
+
+                for (long i = 0; i < max; i++)
+                {
+                    Console.WriteLine(i.ToString().PadLeft(digits, '0'));
+                }
+            }
+        }
+    }
+}
